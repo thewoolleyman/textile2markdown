675f4135718ad0ced0dbc8d7f6d4659d1eff43b6
Chad Woolley <thewoolleyman@gmail.com>
2008-09-06 23:38:12 -0700

    handle lists

--- lib/textile2markdown.rb
+++ lib/textile2markdown.rb
@@ -11,11 +11,23 @@ class Textile2Markdown
   end
   
   def convert(input)
-    return convert_links(input)
+    tmp = convert_links(input)
+    tmp = convert_ul(tmp)
+    tmp = convert_ol(tmp)
+    tmp
   end
   
   protected
+
   def convert_links(input)
     input.gsub(/"(.*)":(.+?)(?=,| |$)/, '[\1](\2)') 
+  end
+
+  def convert_ul(input)
+    input.gsub(/^\*\*\*\*/, '      *').gsub(/^\*\*\*/, '    *').gsub(/^\*\*/, '  *')
+  end
+
+  def convert_ol(input)
+    input.gsub(/^####/, '         1.').gsub(/^###/, '      1.').gsub(/^##/, '   1.').gsub(/^#/, '1.')
   end
 end
--- test/expected.markdown
+++ test/expected.markdown
@@ -1,3 +1,14 @@
-xxx [foo](bar) asdfasdf
+links
+xxx [foo](bar) yyy
 xxx [foo](bar)
-xxx [foo](bar), asdfasdf
+xxx [foo](bar), yyy
+
+* list item
+  * sub list item
+    * sub sub list item
+      * sub sub sub list item
+
+1. list item
+   1. sub list item
+      1. sub sub list item
+         1. sub sub sub list item
